29c7800dc8b4efa2c9e82902393c31f548288aa0
Kzsanjeet <sanjeetkazithapa@gmail.com>
2024-06-01 00:12:23 +0545

    added edit and delete post

--- server/controller/post.js
+++ server/controller/post.js
@@ -70,7 +70,7 @@ const deletePost = async (req, res) => {
       return res.status(200).json({ success: true, message: "Deleted successfully." });
     }
   } catch (error) {
-    return res.status(400).json({ success: false, message: "error", error });
+    return res.status(400).json({ success: false, message: "error", error });///
   }
 };
 

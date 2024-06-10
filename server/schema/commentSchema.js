e8c464c8b3d6ac56da81864dadb3d4635bf276a2
Kzsanjeet <sanjeetkazithapa@gmail.com>
2024-06-10 12:45:28 +0545

    added post details in comment

--- server/schema/commentSchema.js
+++ server/schema/commentSchema.js
@@ -1,14 +1,18 @@
 const mongoose = require("mongoose")
 const RegisterUser = require("./login&register")
+const postData = require("./postSchema")
 
 const addComment = new mongoose.Schema({
     comment : {type:String, require:true},
     "user":{
         type:mongoose.Schema.Types.ObjectId,
         ref: RegisterUser
+    },
+    "post":{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: postData
     }
-})
-
+}) 
 const addUserComment = mongoose.model("addUserComment",addComment)
 
 module.exports = addUserComment

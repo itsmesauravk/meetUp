2f38459221514dda04b15f040eb16262cd1eb772
Kzsanjeet <sanjeetkazithapa@gmail.com>
2024-06-13 14:22:32 +0545

    updated the route for get comment user

--- server/route/commentRoute.js
+++ server/route/commentRoute.js
@@ -3,7 +3,7 @@ const express = require("express")
 const commentRouter = express.Router()
 
 commentRouter.route("/add-comment/:userId/:postId").post(addComment)
-commentRouter.route("/get-comment-user-data/:postId").get(commentUser)
+commentRouter.route("/get-comment-user-data").get(commentUser)
 commentRouter.route("/edit-users-comment/:id").patch(editComment)
 commentRouter.route("/delete-users-comment/:id").delete(deleteComment)
 
